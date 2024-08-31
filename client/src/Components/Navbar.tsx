bb709f780319ff7621d8784b560c2b89ab3043b0
2OldForThis2 <145152903+2OldForThis2@users.noreply.github.com>
2024-08-31 13:59:03 -0400

    update navbar

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -18,10 +18,10 @@ export default function Navbar() {
       {/* navbar */}
       <nav className="bg-yellow-400 opacity-80">
         <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
+          <div className="flex items-center justify-center h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
-                <Link to="/" className="text-white font-bold text-2xl">
+                <Link to="/" className="text-black font-bold text-2xl">
                   WHOOGA!
                 </Link>
               </div>
@@ -31,22 +31,16 @@ export default function Navbar() {
             <div className="hidden md:block">
               <div className="ml-4 flex items-center space-x-4">
                 <Link
-                  to="/"
-                  className="text-white hover:bg-yellow-300 hover:text-orange-400 rounded-lg p-2 font-bold"
-                >
-                  Home
-                </Link>
-                <Link
                   to="/login"
-                  className="text-white hover:bg-yellow-300 hover:text-orange-400 rounded-lg p-2 font-bold"
+                  className="text-black hover:bg-yellow-300 hover:text-orange-400 rounded-lg p-2 font-bold"
                 >
                   Log in
                 </Link>
                 <Link
                   to="/signup"
-                  className="text-white hover:bg-yellow-300 hover:text-orange-400 rounded-lg p-2 font-bold"
+                  className="text-black bg-white hover:bg-yellow-300 hover:text-orange-400 rounded-lg p-2 font-bold"
                 >
-                  Sign up
+                  Register
                 </Link>
               </div>
             </div>
@@ -54,7 +48,7 @@ export default function Navbar() {
             {/* responsive navbar */}
             <div className="md:hidden flex items-center">
               <button
-                className="inline-flex items-center justify-center p-2 pr-8 rounded-md text-white
+                className="inline-flex items-center justify-center p-2 pr-8 rounded-md text-black
                               hover:text-orange-400 focus:ring-2 focus:ring-inset focus:ring-white"
                 onClick={toggleNav}
               >
@@ -97,22 +91,16 @@ export default function Navbar() {
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               <Link
-                to="/"
-                className="block text-white hover:bg-yellow-300 hover:text-orange-400 rounded-lg p-2 font-bold"
-              >
-                Home
-              </Link>
-              <Link
                 to="/login"
-                className="block text-white hover:bg-yellow-300 hover:text-orange-400 rounded-lg p-2 font-bold"
+                className="block text-black hover:bg-yellow-300 hover:text-orange-400 rounded-lg p-2 font-bold"
               >
                 Log in
               </Link>
               <Link
                 to="/signup"
-                className="block text-white hover:bg-yellow-300 hover:text-orange-400 rounded-lg p-2 font-bold"
+                className="block text-black bg-white hover:bg-yellow-300 hover:text-orange-400 rounded-lg p-2 font-bold"
               >
-                Sign up
+                Register
               </Link>
             </div>
           </div>
