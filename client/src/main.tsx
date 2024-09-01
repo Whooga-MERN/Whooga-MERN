c48f0ef6fbe35bce339fc404b469638cdc4dd9aa
2OldForThis2 <145152903+2OldForThis2@users.noreply.github.com>
2024-09-01 17:06:23 -0400

    update route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,16 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 import Home from "./Landing/landing";
 import Profile from "./Profile/profile";
-// import Signup from "./Signup/signup";
-// import Login from "./Login/login";
+import Signup from "./Signup/signup";
+import Login from "./Login/login";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/home" element={<Home />} />
-      {/* <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
   </BrowserRouter>
