7429a90ade83ec4528c8275a1d010aecb64c12e4
Katoromic <katdesigns14@gmail.com>
2024-09-03 13:22:35 -0400

    Finished bulk of collections page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -15,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/collections" element={<Collections />} />
     </Routes>
   </BrowserRouter>
 );
