093f21cf1aa326c1cb08523775e3af50643bfdde
2OldForThis2 <145152903+2OldForThis2@users.noreply.github.com>
2024-10-10 16:28:30 -0500

    update search bar

--- client/src/Components/searchBar.tsx
+++ client/src/Components/searchBar.tsx
@@ -1,9 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import { IoMdArrowDropdown } from "react-icons/io";
-import { useQuery } from "react-query";
-import debounce from "lodash.debounce";
-import { fetchSearchResults } from "../utils/fetchSearchResults";
+import { IoMdArrowDropdown, IoMdClose } from "react-icons/io";
 
 interface SearchBarProps {
   attributes: string[];
@@ -11,89 +8,116 @@ interface SearchBarProps {
   handleError: (error: any) => void;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({ attributes, handleError }) => {
+const SearchBar: React.FC<SearchBarProps> = ({
+  attributes,
+  fetchSearchResults,
+  handleError,
+}) => {
   const [selectedOption, setSelectedOption] = useState<string>("Options");
   const [searchTerm, setSearchTerm] = useState<string>("");
-  const [debouncedSearchTerm, setDebouncedSearchTerm] =
-    useState<string>(searchTerm);
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [searchTags, setSearchTags] = useState<string[]>([]);
 
   const handleOptionSelect = (option: string) => {
     setSelectedOption(option);
     setIsDropdownOpen(false);
   };
 
-  useEffect(() => {
-    const debounced = debounce(() => {
-      setDebouncedSearchTerm(searchTerm);
-    }, 500); // 500ms delay
-
-    debounced();
-
-    return () => {
-      debounced.cancel();
-    };
-  }, [searchTerm]);
-
-  const {
-    data: searchResults,
-    isLoading: isLoadingSearch,
-    isFetching: isFetchingSearch,
-    isError: searchIsError,
-    error: searchError,
-  } = useQuery(
-    ["search", selectedOption, debouncedSearchTerm],
-    () => fetchSearchResults(selectedOption, debouncedSearchTerm),
-    {
-      enabled: selectedOption !== "Options" && debouncedSearchTerm.length > 0,
-      onError: handleError,
+  const handleEnterPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && searchTerm.trim()) {
+      e.preventDefault();
+      const newTag = `${selectedOption}: ${searchTerm.trim()}`;
+      if (!searchTags.includes(newTag)) {
+        setSearchTags([...searchTags, newTag]);
+        setSearchTerm("");
+      }
     }
-  );
+  };
+
+  const deleteSearchTag = (tag: string) => {
+    setSearchTags(searchTags.filter((t) => t !== tag));
+  };
+
+  const handleResetTags = () => {
+    setSearchTags([]);
+  };
 
   return (
-    <div className="relative border-none">
-      {/* search */}
-      <input
-        type="search"
-        placeholder="What are you looking for today?"
-        className="border-none ring-1 ring-gray-200 w-full lg:w-[400px] p-4 rounded-lg pl-32"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-
-      {/* dropdown*/}
-      <div className="absolute inset-y-0 left-0 flex items-center">
+    <div className="flex flex-col justify-center mt-8">
+      {/* Search Bar */}
+      <div className="relative items-center">
+        {/* Dropdown Button */}
+        <div className="absolute inset-y-0 left-0 flex items-center">
+          <button
+            id="dropdown-button"
+            className="flex-shrink-0 z-10 inline-flex items-center h-full px-4 text-md font-semibold text-black bg-yellow-300 border rounded-l-lg hover:bg-yellow-200 dark:bg-yellow-100 dark:hover:bg-yellow-200 dark:text-black"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          >
+            {selectedOption}
+            <IoMdArrowDropdown className="text-2xl pl-1" />
+          </button>
+        </div>
+        {/* Input Box */}
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleEnterPress}
+          placeholder="What are you looking for today?"
+          className="flex-grow border-none ring-1 ring-gray-200 w-96 p-4 rounded-lg pl-32"
+        />
+        {/* Search Button */}
         <button
-          id="dropdown-button"
-          className="flex-shrink-0 z-10 inline-flex items-center h-full px-4 text-md font-semibold text-black bg-yellow-300 border rounded-l-lg hover:bg-yellow-200 dark:bg-yellow-100 dark:hover:bg-yellow-200 dark:text-black"
-          style={{ height: "100%" }}
-          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          className="btn text-lg text-black bg-yellow-300 hover:bg-yellow-200 rounded-full px-4 py-2 ml-2"
+          onClick={() => {
+            console.log("Search Button Clicked");
+          }}
         >
-          {selectedOption}
-          <IoMdArrowDropdown className="text-2xl pl-1" />
+          Search
+          <FaMagnifyingGlass />
         </button>
+
+        {/* Dropdown List */}
+        {isDropdownOpen && (
+          <div className="absolute left-0 top-full mt-1 bg-white border border-gray-200 rounded-md shadow-lg w-48 z-10">
+            <ul className="py-1">
+              {attributes.map((attribute) => (
+                <li
+                  key={attribute}
+                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handleOptionSelect(attribute)}
+                >
+                  {attribute}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
 
-      <button className="absolute right-1 top-1/2 -translate-y-1/2 p-4 rounded-full">
-        <FaMagnifyingGlass />
-      </button>
+      {/* Search Tags */}
+      <div className="flex flex-wrap mt-2">
+        {searchTags.map((tag, index) => (
+          <div
+            key={index}
+            className="flex items-center bg-primary text-black font-semibold text-lg rounded-md pl-4 pr-2 py-1 mr-3 mt-2"
+          >
+            {tag}
+            <button className="pl-4" onClick={() => deleteSearchTag(tag)}>
+              <IoMdClose />
+            </button>
+          </div>
+        ))}
 
-      {/* dropdown list */}
-      {isDropdownOpen && (
-        <div className="absolute left-0 mt-2 bg-white border border-gray-200 rounded-md shadow-lg w-48 z-10">
-          <ul className="py-1">
-            {attributes.map((attribute) => (
-              <li
-                key={attribute}
-                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                onClick={() => handleOptionSelect(attribute)}
-              >
-                {attribute}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+        {searchTags.length > 0 && (
+          <button
+            className="ml-3 text-lg text-black bg-yellow-400 !important rounded-full px-4 py-2 mt-2"
+            onClick={handleResetTags}
+          >
+            Reset
+          </button>
+        )}
+      </div>
     </div>
   );
 };
