d429b9bc4edf906363284a1202ec3f5c259292c9
2OldForThis2 <145152903+2OldForThis2@users.noreply.github.com>
2024-10-20 13:56:52 -0400

    add s

--- client/src/utils/utils.tsx
+++ client/src/utils/utils.tsx
@@ -10,7 +10,8 @@ export function buildPath(route: string): string {
   // if (!process && !process.env && process.env.NODE_ENV === "production") {
   if (process.env.NODE_ENV === "production") {
     return (
-      "http://whooga-env-1.eba-xvanh3td.us-east-1.elasticbeanstalk.com/" + route
+      "https://whooga-env-1.eba-xvanh3td.us-east-1.elasticbeanstalk.com/" +
+      route
     );
   } else {
     return "http://localhost:3000/" + route;
