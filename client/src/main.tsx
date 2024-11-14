adfd5d60175fc63bfd3dc3dd65fb640933227b3b
lukeMaggio24 <139585492+lukeMaggio24@users.noreply.github.com>
2024-11-14 18:45:54 -0500

    profile and optin page done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -25,6 +25,7 @@ import BulkUpload from "./Pages/bulk_upload.tsx";
 import BulkUploadStep2 from "./Pages/bulk_upload_step_2.tsx";
 import PrivacyPolicy from "./Pages/privacypolicy.tsx";
 import ContactUs from "./Pages/contactus.tsx";
+import OptIn from "./Pages/OptIn.tsx";
 
 import "./custom-amplify-styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -57,6 +58,7 @@ createRoot(document.getElementById("root")!).render(
           {/* new routes for the Privacy Policy and Contact Us pages */}
           <Route path="/privacypolicy" element={<PrivacyPolicy />} />
           <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/optin" element={<OptIn />} />
 
           {/* authenticated routes */}
           <Route
