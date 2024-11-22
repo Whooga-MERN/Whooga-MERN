51d3b4ceb9b3c4f5c31512bc7eba26ed508729db
Nelson Herrera <ndanielherrera@icloud.com>
2024-11-22 00:00:06 -0500

    Implemented Collection name edit and Collection pic edit

--- client/src/Pages/collections.tsx
+++ client/src/Pages/collections.tsx
@@ -12,6 +12,9 @@ import fetchJWT from "../fetchJWT";
 import { fetchCollectionSearchResults } from "../utils/collectionsPage";
 import { buildPath } from "../utils/utils";
 import Footer from "../Components/Footer";
+import { FaRegEdit } from "react-icons/fa";
+import { PhotoIcon } from "@heroicons/react/24/solid";
+import { set } from "lodash";
 
 export default function Collections() {
   const [user, setUser] = useState<any>(null);
@@ -157,6 +160,8 @@ export default function Collections() {
     }
   }, [isUserIdFetched, userId, JWT]);
 
+
+
   function handleClick(collectionUniverseId: any) {
     console.log("collectionUniverseId: ", collectionUniverseId);
     console.log("collections: ", collections);
@@ -215,6 +220,201 @@ export default function Collections() {
       await fetchCollectionSearchResults(debouncedSearchTerm, userId),
     enabled: debouncedSearchTerm.length > 0,
   });
+
+
+  // ======== Edit Collection Name and Image =========
+  const [currentCollectionEdit, setCurrentCollectionEdit] = useState<any>(null);
+  const [openCollectionEditModal, setOpenCollectionEditModal] = useState(false);
+  const [formName, setFormName] = useState("");
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [nameError, setNameError] = useState(false);
+  const [imageError, setImageError] = useState(false);
+
+
+  const refetchCollections = async () => {
+    // fetch collections
+    if (isUserIdFetched && userId) {
+      console.log("in fetch collections ", userId);
+      const fetchCollections = async () => {
+        const response = await fetch(buildPath(`collection/user/${userId}`), {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${JWT}`,
+          },
+        });
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.error);
+        }
+        const data = await response.json();
+        console.log("collections as data: ", data);
+        console.log("1st collection:", data[0].collection_pic);
+
+        setCollections(
+          data.map((col: any) => {
+            return {
+              name: col.name,
+              id: col.collection_id,
+              collectionUniverseId: col.collection_universe_id,
+              image_url: col.collection_pic,
+              description: "",
+              newListing: false,
+              customAttributes: col.custom_attributes,
+              favoriteAttributes: col.favorite_attributes,
+              hiddenAttributes: col.hidden_attributes,
+            };
+          })
+        );
+
+        const collectionIds = data.map(
+          (col: { collection_id: string; collection_universe_id: string }) => [
+            col.collection_id,
+            col.collection_universe_id,
+          ]
+        );
+        console.log("collectionIds: ", collectionIds);
+        localStorage.setItem("collectionIds", JSON.stringify(collectionIds));
+
+        console.log("Collections as collection:", collections);
+      };
+
+      fetchCollections();
+    }
+  };
+
+  function openEditCollection(collectionUniverseId: number) {
+    console.log("Edit Collection");
+
+    console.log("collectionUniverseId: ", collectionUniverseId);
+
+
+    // Get the collection
+    const collection = collections.find(
+      (col) => col.collectionUniverseId === collectionUniverseId
+    );
+
+    console.log("collection upon being clicked: ", collection);
+
+    // Set the collection in local storage
+    if (collection) {
+      setCurrentCollectionEdit(collection);
+      setFormName(collection.name);
+    } else {
+      console.error("Collection not found")
+    }
+
+    // Open the modal
+    setOpenCollectionEditModal(true);
+  }
+
+  function closeEditCollection() {
+    setCurrentCollectionEdit(null);
+    setOpenCollectionEditModal(false);
+  }
+
+
+  // handle file upload
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setImageFile(e.target.files[0]);
+    }
+  };
+
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormName(value);
+  };
+
+
+  const handleEditCollectionSubmit = async () => {
+    // hit endpoint
+    // update collection name and image
+
+
+    console.log("currentCollectionEdit: ", currentCollectionEdit);
+    console.log("formName: ", formName);
+    console.log("imageFile: ", imageFile);
+
+
+    if (formName === "") {
+      setNameError(true);
+      return;
+    }
+
+
+    if (formName !== "" && formName !== currentCollectionEdit.name) {
+      // update collection name
+      try{
+        const response =  await fetch(buildPath(`collection-universe/update-collection-universe-name`), {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${JWT}`,
+          },
+          body: JSON.stringify({
+            newUniverseName: formName,
+            collectionUniverseId: currentCollectionEdit.collectionUniverseId,
+          }),
+        });
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.error);
+        } else {
+          localStorage.setItem("showSuccessAlert", "true");
+          localStorage.setItem("alertMessage", "Collection name updated successfully");
+          window.location.reload();
+        }
+      } catch (error) {
+        console.error("Error updating collection name: ", error);
+      }
+    }
+
+    // update collection image
+
+    if (imageFile === null) {
+      console.log("imageFile: ", imageFile);
+    }
+
+    if (imageFile !== null) {
+      try{
+        const request = new FormData();
+        request.append("coverImage", imageFile as Blob);
+        request.append("collectionUniverseId", currentCollectionEdit.collectionUniverseId);
+  
+        const response =  await fetch(buildPath(`collection-universe/update-universe-pic`), {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${JWT}`,
+          },
+          body: request,
+        });
+  
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.error);
+        } else {
+          localStorage.setItem("showSuccessAlert", "true");
+          localStorage.setItem("alertMessage", "Collection image updated successfully");
+          window.location.reload();
+        }
+  
+      } catch (error) {
+        console.error("Error updating collection image: ", error);
+      }
+    }
+    
+
+    // close modal
+    await refetchCollections();
+    closeEditCollection();
+    window.location.reload();
+  }
 
   return (
     <>
@@ -371,8 +571,17 @@ export default function Collections() {
                       alt={collection.name}
                     />
                   </figure>
-                  <div className="card-body">
+                  <div className="card-body flex flex-row justify-center items-center">
                     <h2 className="card-title">{collection.name}</h2>
+                    <button
+                      className="px-3 py-1 bg-orange-300 text-[#7b4106] hover:text-white rounded-full"
+                      onClick={(e) => {
+                        e.stopPropagation(); // Prevent event from bubbling to the parent <div>
+                        openEditCollection(collection.collectionUniverseId);
+                      }}
+                    >
+                      <FaRegEdit />
+                    </button>
                   </div>
                 </div>
               </div>
@@ -381,6 +590,79 @@ export default function Collections() {
             <div className="text-center">No collections found</div>
           )}
         </div>
+
+
+        {openCollectionEditModal && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" 
+            // onClick={closeEditCollection}
+          >
+            <div className="bg-white dark:bg-gray-800 rounded-lg p-8 sm:w-3/4 lg:w-[480px] max-h-screen overflow-y-auto mt-20">
+              <label className="block text-gray-700 dark:text-gray-300 text-lg font-bold mb-2">
+                {/* {attribute.charAt(0).toUpperCase() + attribute.slice(1)} */}
+                Change Collection Name
+                {/* {currentCollectionEdit && currentCollectionEdit.name} */}
+              </label>
+              <p className="block text-gray-500 dark:text-gray-300 text-sm mb-2"><span className="font-bold text-gray-600">Reminder:</span> A collection's name is used for notifications </p>
+              {nameError && (<p className="block text-red-500 dark:text-red-300 text-sm mb-2">A collection name must be greater than 3 characters </p>)}
+              <input
+                type="text"
+                name={"collectionName"}
+                value={formName || ""}
+                placeholder={`${currentCollectionEdit.name}`}
+                onChange={handleChange}
+                className="border rounded w-full py-2 px-3 text-gray-700"
+              />
+              <label
+                htmlFor="cover-photo"
+                className="block text-gray-700 dark:text-gray-300 text-lg font-bold mb-2 mt-4"
+              >
+                Upload New Collection Photo
+              </label>
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 dark:bg-slate-300 px-6 py-10">
+                <div className="text-center">
+                  <PhotoIcon
+                    aria-hidden="true"
+                    className="mx-auto h-12 w-12 text-gray-300 dark:text-gray-400"
+                  />
+                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md px-2 bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                    >
+                      <span>Upload a photo</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        className="sr-only"
+                        onChange={handleFileChange}
+                      />
+                    </label>
+                    <p>or drag and drop</p>
+                  </div>
+                  <p className="text-xs leading-5 text-gray-600">
+                    PNG, JPG
+                  </p>
+                </div>
+              </div>
+              <div className="flex justify-end space-x-4 mt-8">
+                <button
+                  type="button"
+                  onClick={closeEditCollection}
+                  className="bg-gray-300 hover:bg-yellow-300 text-black font-bold py-2 px-4 rounded-xl"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={handleEditCollectionSubmit}
+                  className="bg-yellow-400 hover:bg-yellow-300 text-black font-bold py-2 px-4 rounded-xl"
+                >
+                  Save Changes
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
       <Footer />
 
